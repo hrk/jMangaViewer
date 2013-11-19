739db75f69ec55f00456985f6b4ce16e670067c2
Luca Santarelli <luca.santarelli@gmail.com>
2013-11-19 15:13:39 +0100

    Cleaned up old code.

--- src/it/sineo/jMangaViewer/util/PatternFormatter.java
+++ src/it/sineo/jMangaViewer/util/PatternFormatter.java
@@ -3,35 +3,28 @@
  */
 package it.sineo.jMangaViewer.util;
 
+import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 /**
- * @author DMTLS (ing. Luca Santarelli @ Datamat S.p.A)
- *
+ * @author Luca Santarelli
+ * 
  */
 public class PatternFormatter extends Formatter {
 
-  private final static String defaultPattern = "%d{HH:mm:ss.SSS} %-5p %C{1}#%M(%L): %m%n"; 
+	private SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS");
 
-  private SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS");
-
-  /* (non-Javadoc)
-   * @see java.util.logging.Formatter#format(java.util.logging.LogRecord)
-   */
-  public String format(LogRecord record) {
-    
-    StringBuffer sb = new StringBuffer(defaultPattern.length() * 3);
-    Date d = new Date(record.getMillis());
-    synchronized (sdf) {
-      sb.append(sdf.format(d));
-    }
-    sb.append(" ").append(record.getLevel().getLocalizedName());
-    sb.append(" ").append(record.getSourceClassName());
-    sb.append("#").append(record.getSourceMethodName());
-    sb.append("(n/a): ").append(record.getMessage()).append("\n");
-    return sb.toString();
-  }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.logging.Formatter#format(java.util.logging.LogRecord)
+	 */
+	public String format(LogRecord record) {
+		return MessageFormat.format("{0} {1} {2}#{3}(n/a): {4}\n",
+				sdf.format(record.getMillis()), record.getLevel(),
+				record.getSourceClassName(), record.getSourceMethodName(),
+				record.getMessage());
+	}
 }
