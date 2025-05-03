5f9ec4f4912590f80cc6d2d8dc8e9cbd8f4e4b2c
Luca Santarelli <luca.santarelli@gmail.com>
2025-05-03 21:25:46 +0200

    Remove unused code.

--- src/it/sineo/jMangaViewer/ComicBook.java
+++ src/it/sineo/jMangaViewer/ComicBook.java
@@ -1,51 +1,16 @@
 package it.sineo.jMangaViewer;
 
-import java.awt.Image;
-import java.io.IOException;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.List;
-
-import javax.imageio.ImageIO;
 
 public class ComicBook {
 
 	private List<URL> imageUrls;
 	private int currentIdx;
 
-	private transient HashMap<URL, Image> images;
-	private transient Thread workerThread;
-
 	public ComicBook(List<URL> urls) {
 		imageUrls = urls;
 		currentIdx = 0;
-
-		images = new HashMap<URL, Image>(imageUrls.size());
-		try {
-			Image firstImage = ImageIO.read(imageUrls.get(0));
-			images.put(imageUrls.get(0), firstImage);
-		} catch (IOException ioex) {
-			System.err.println(ioex.getMessage());
-		}
-
-		workerThread = new Thread(new Runnable() {
-
-			public void run() {
-				for (URL url : imageUrls) {
-					try {
-						Image image = ImageIO.read(url);
-						synchronized (images) {
-							images.put(url, image);
-						}
-					} catch (Exception ex) {
-						ex.printStackTrace();
-					}
-				}
-				notifyAll();
-			}
-		});
-		// workerThread.start();
-
 	}
 
 	public URL getFirstPageURL() {
@@ -58,42 +23,12 @@ public class ComicBook {
 		return imageUrls.get(currentIdx);
 	}
 
-	public Image getFirstPage() {
-		return images.get(getFirstPageURL());
-	}
-
 	public URL getNextPageURL() {
 		return imageUrls.get(++currentIdx);
 	}
 
-	public Image getNextPage() {
-		if (workerThread.isAlive()) {
-			System.out.println("THREAD ANCORA VIVO, MI METTO IN WAIT");
-			try {
-				wait();
-				System.out.println("SONO USCITO DALLA WAIT");
-			} catch (InterruptedException iex) {
-				iex.printStackTrace();
-			}
-		}
-		return images.get(getNextPageURL());
-	}
-
 	public URL getPreviousPageURL() {
 		return imageUrls.get(--currentIdx);
-	}
-
-	public Image getPreviousImage() {
-		if (workerThread.isAlive()) {
-			System.out.println("THREAD ANCORA VIVO, MI METTO IN WAIT");
-			try {
-				wait();
-				System.out.println("SONO USCITO DALLA WAIT");
-			} catch (InterruptedException iex) {
-				iex.printStackTrace();
-			}
-		}
-		return images.get(getPreviousPageURL());
 	}
 
 	public boolean hasNextPage() {
@@ -114,32 +49,6 @@ public class ComicBook {
 
 	public int getCurrentPageNumber() {
 		return currentIdx + 1;
-	}
-
-	public Image getCurrentPage() {
-		if (workerThread.isAlive()) {
-			System.out.println("THREAD ANCORA VIVO, MI METTO IN WAIT");
-			try {
-				wait();
-				System.out.println("SONO USCITO DALLA WAIT");
-			} catch (InterruptedException iex) {
-				iex.printStackTrace();
-			}
-		}
-		return images.get(getCurrentPageURL());
-	}
-
-	public Image getImage(URL url) {
-		if (workerThread.isAlive()) {
-			System.out.println("THREAD ANCORA VIVO, MI METTO IN WAIT");
-			try {
-				wait();
-				System.out.println("SONO USCITO DALLA WAIT");
-			} catch (InterruptedException iex) {
-				iex.printStackTrace();
-			}
-		}
-		return images.get(url);
 	}
 
 }
